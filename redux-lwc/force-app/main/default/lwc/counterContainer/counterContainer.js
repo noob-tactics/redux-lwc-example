29d7184af8af7b685d8f39100856c031efc79f5f
kprabuddh <kprabuddh@email.com>
2023-01-26 23:31:56 +0530

    Adding counter cmp

---
+++ redux-lwc/force-app/main/default/lwc/counterContainer/counterContainer.js
@@ -0,0 +1,25 @@
+import { LightningElement, api } from 'lwc';
+import {createStore, combineReducers, createLogger} from 'c/lwcRedux';
+import reducers from 'c/counterReducers';
+
+// Set ENABLE_LOGGING true if you wanna use the logger.
+// We prefer to use the Custom label because we can directly access in the LWC components.
+const ENABLE_LOGGING = true;
+
+export default class CounterContainer extends LightningElement {
+    @api store;
+    initialize(){
+        let logger;
+        
+        // Check for the Logging
+        if(ENABLE_LOGGING){
+            logger = createLogger({
+                duration: true,
+                diff: true
+            });
+        }
+        
+        const combineReducersInstance = combineReducers(reducers);
+        this.store = createStore(combineReducersInstance, logger);
+    }
+}
